b5a58814d45e60c31b98caa4205c2710cf8e7398
Cloud User <ec2-user@ip-172-31-46-29.ec2.internal>
2019-04-01 06:39:33 +0000

    hello

---
+++ jenkins.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+yum update -y
+#!/bin/bash
+yum update -y
+yum install git
+yum install java-1.8.0-openjdk* -y
+yum install wget
+yum install unzip,zip
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+yum install jenkins -y
+systemctl start jenkins
+systemctl enable jenkins
+
+
+
+
